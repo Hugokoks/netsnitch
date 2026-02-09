5169132b2f8ac712ff2da918f6f97904382be899
David <david@localhost.localdomain>
2026-02-09 11:28:30 +0100

    arp timeout flag added

--- internal/scans/arp_active/input.go
+++ internal/scans/arp_active/input.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"netsnitch/internal/domain"
 	"netsnitch/internal/input"
+	"time"
 )
 
 type Parser struct{}
@@ -15,19 +16,40 @@ func (Parser) Protocol() domain.Protocol {
 }
 
 func (Parser) Parse(tokens []string) (input.Stage, error) {
-	if len(tokens) != 2 {
-		return input.Stage{}, fmt.Errorf("usage: arp <cidr|ip[,ip]>")
+
+	if len(tokens) < 2 {
+		return input.Stage{}, fmt.Errorf("usage: arp <cidr>")
 	}
 
-	scope, err := domain.ParseScope(tokens[1])
+	flags, rest, err := input.ExtractFlags(tokens[1:])
 
 	if err != nil {
 		return input.Stage{}, err
 	}
 
+	if len(rest) != 1 {
+		return input.Stage{}, fmt.Errorf("exactly one target scope required")
+	}
+
+	//// ---- scope ----
+	scope, err := domain.ParseScope(rest[0])
+	if err != nil {
+		return input.Stage{}, err
+	}
+
+	// ----timeout-----
+	timeout := domain.DefaultTimeout
+
+	if t, ok := flags["t"]; ok {
+		if d, err := time.ParseDuration(t); err == nil {
+			timeout = d
+		}
+	}
+
 	return input.Stage{
 		Protocol: domain.ARP_ACTIVE,
 		Scope:    scope,
+		Timeout:  timeout,
 	}, nil
 }
 
